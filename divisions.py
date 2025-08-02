759d9aa326574fbcef87124dd91e2b58d3db1541
5Santiago <107329231+5Santiago@users.noreply.github.com>
2025-08-02 15:54:40 +0000

    Created divisions.py

---
+++ divisions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+def divisions(x,y):
+    x=int(x)
+    y=int(y)
+    if y == 0:
+        return "The second number can't be 0"
+    else:
+        return x/y
+
+
+print(divisions(sys.argv[1],sys.argv[2]))
